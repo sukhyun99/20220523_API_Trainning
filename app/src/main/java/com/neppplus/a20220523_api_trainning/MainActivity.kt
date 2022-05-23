e9e989513fdb0971a87c482651c47f3347a1a4b5
weekendHuman <buddah99@naver.com>
2022-05-23 10:08:28 +0900

    바인딩 연결 / 로그인 페이지 xml 구현 / id,pw 변수 생성

--- app/src/main/java/com/neppplus/a20220523_api_trainning/MainActivity.kt
+++ app/src/main/java/com/neppplus/a20220523_api_trainning/MainActivity.kt
@@ -2,10 +2,28 @@ package com.neppplus.a20220523_api_trainning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.a20220523_api_trainning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvent()
+    }
+
+
+    fun setupEvent() {
+        binding.loginBtn.setOnClickListener {
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPw = binding.passwordEdt.text.toString()
+        }
+    }
+
+    fun setValues() {
+
     }
 }
